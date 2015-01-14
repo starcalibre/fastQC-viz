799084692725da7a6665b14b85e077a07adcd448
Luke Shillabeer <lshillabeer@gmail.com>
2015-01-14 17:55:45 +1100

    Added all modules in fastQC output file to parser.

--- public/js/fastqc.js
+++ public/js/fastqc.js
@@ -94,6 +94,154 @@ FastQCParser.prototype.parseQC = function() {
         curLine = lines[i].split('\t');
       }
     }
+
+    // Parse the per-sequence GC content section of the FASTQC
+    // output file and merge all parsed lines into seq_gc_content object
+    else if(curLine[0] === '>>Per sequence GC content') {
+      var seq_gc_content = {};
+      seq_gc_content['status'] = curLine[1];
+      seq_gc_content['gc'] = [];
+      i += 2;
+      curLine = lines[i].split('\t');
+
+      while (curLine[0] !== '>>END_MODULE') {
+        var gc_content = {};
+        gc_content['gc'] = curLine[0];
+        gc_content['count'] = curLine[1];
+        seq_gc_content['gc'].push(gc_content);
+        i += 1;
+        curLine = lines[i].split('\t');
+      }
+    }
+
+    // Parse the per-sequence N content section of the FASTQC
+    // output file and merge all parsed lines into seq_n_content object
+    else if(curLine[0] === '>>Per base N content') {
+      var seq_n_content = {};
+      seq_n_content['status'] = curLine[1];
+      seq_n_content['n'] = [];
+      i += 2;
+      curLine = lines[i].split('\t');
+
+      while (curLine[0] !== '>>END_MODULE') {
+        var n_content = {};
+        n_content['n'] = curLine[0];
+        n_content['count'] = curLine[1];
+        seq_n_content['n'].push(n_content);
+        i += 1;
+        curLine = lines[i].split('\t');
+      }
+    }
+
+
+    // Parse the sequence length distribution section of the FASTQC
+    // output file and merge all parsed lines into seq_lengths object
+    else if(curLine[0] === '>>Sequence Length Distribution') {
+      var seq_lengths = {};
+      seq_lengths['status'] = curLine[1];
+      seq_lengths['lengths'] = [];
+      i += 2;
+      curLine = lines[i].split('\t');
+
+      while (curLine[0] !== '>>END_MODULE') {
+        var length = {};
+        length['length'] = curLine[0];
+        length['count'] = curLine[1];
+        seq_lengths['lengths'].push(length);
+        i += 1;
+        curLine = lines[i].split('\t');
+      }
+    }
+
+    // Parse the per-sequence N content section of the FASTQC
+    // output file and merge all parsed lines into seq_n_content object
+    else if(curLine[0] === '>>Sequence Duplication Levels') {
+      var seq_duplication = {};
+      seq_duplication['status'] = curLine[1];
+      seq_duplication['duplication'] = [];
+      
+      i += 1;
+      curLine = lines[i].split('\t');
+      seq_duplication['total_dupe_perc'] = curLine[1]
+      
+      i += 2;
+      curLine = lines[i].split('\t');
+
+      while (curLine[0] !== '>>END_MODULE') {
+        var duplication = {};
+        duplication['level'] = curLine[0];
+        duplication['perc_dedupe'] = curLine[1];
+        duplication['perc_total'] = curLine[2];
+        seq_duplication['duplication'].push(duplication);
+        i += 1;
+        curLine = lines[i].split('\t');
+      }
+    }
+
+    // Parse the per-sequence N content section of the FASTQC
+    // output file and merge all parsed lines into seq_n_content object
+    else if(curLine[0] === '>>Overrepresented sequences') {
+      var overrep_seqs = {};
+      seq_duplication['status'] = curLine[1];
+      seq_duplication['overrep'] = [];
+      
+      i += 1;
+      curLine = lines[i].split('\t');
+
+      while (curLine[0] !== '>>END_MODULE') {
+        var overrep_seq = {};
+        overrep_seq['level'] = curLine[0];
+        overrep_seq['perc_dedupe'] = curLine[1];
+        seq_duplication['duplication'].push(duplication);
+        i += 1;
+        curLine = lines[i].split('\t');
+      }
+    }
+
+    // Parse the adapter content section of the FASTQC output file
+    // and merge all parsed lines into adapter_content object
+    else if(curLine[0] === '>>Adapter Content') {
+      var adapter_content = {};
+      adapter_content['status'] = curLine[1];
+      adapter_content['content'] = [];
+      
+      i += 2;
+      curLine = lines[i].split('\t');
+
+      while (curLine[0] !== '>>END_MODULE') {
+        var cur_position = {};
+        cur_position['position'] = curLine[0];
+        cur_position['ill_universal'] = curLine[1];
+        cur_position['ill_small_rna'] = curLine[2];
+        cur_position['nextera_trans'] = curLine[3];
+        adapter_content['content'].push(cur_position);
+        i += 1;
+        curLine = lines[i].split('\t');
+      }
+    }
+
+    // Parse the kmer content of the FASTQC output file
+    // and merge all parsed lines into kmer_content object
+    else if(curLine[0] === '>>Kmer Content') {
+      var kmer_content = {};
+      kmer_content['status'] = curLine[1];
+      kmer_content['kmer'] = [];
+      
+      i += 2;
+      curLine = lines[i].split('\t');
+
+      while (curLine[0] !== '>>END_MODULE') {
+        var cur_kmer = {};
+        cur_kmer['sequence'] = curLine[0];
+        cur_kmer['count'] = curLine[1];
+        cur_kmer['pvalue'] = curLine[2];
+        cur_kmer['obs_exp_max'] = curLine[3];
+        cur_kmer['max_obs_exp_pos'] = curLine[4];
+        kmer_content['kmer'].push(cur_kmer);
+        i += 1;
+        curLine = lines[i].split('\t');
+      }
+    }
   }
 
   // combine the parsed section objects into a single module object/array
@@ -101,4 +249,10 @@ FastQCParser.prototype.parseQC = function() {
   this.modules['qual'] = qual_module;
   this.modules['score'] = seq_qual_module;
   this.modules['base_qual_dist'] = base_qual_module; 
+  this.modules['seq_gc_content'] = seq_gc_content;
+  this.modules['seq_n_content'] = seq_n_content;
+  this.modules['seq_lengths'] = seq_lengths;
+  this.modules['seq_duplication'] = seq_duplication;
+  this.modules['adapter_content'] = adapter_content;
+  this.modules['kmer_content'] = kmer_content;
 };
